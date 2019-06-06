687c670fc4c2c296e262bd7fc6025e241ffc5a46
Bruno Albertini <balbertini@usp.br>
2019-06-06 16:25:24 -0300

    Memories TB.

--- vhdl_modules/memory/memorias_tb.vhd
+++ vhdl_modules/memory/memorias_tb.vhd
@@ -29,27 +29,48 @@ architecture testbench of memorias_tb is
       data : out bit_vector(word_size-1 downto 0)
     );
   end component;
-
+  component ram is
+    generic(
+      address_size : natural;
+      word_size    : natural
+    );
+    port(
+      ck, wr : in  bit;
+      addr   : in  bit_vector(address_size-1 downto 0);
+      data_i : in  bit_vector(word_size-1 downto 0);
+      data_o : out bit_vector(word_size-1 downto 0)
+    );
+  end component;
   -- sinais de suporte
   signal a5: bit_vector(4 downto 0);
-  signal d4a, d4b: bit_vector(3 downto 0);
-  -- signal stopc, clk, rst, done: bit := '0';
+  signal d4a, d4b, d4ci, d4co, d4d: bit_vector(3 downto 0);
+  signal stopc, clk, wr: bit := '0';
   -- Periodo do clock
-  -- constant periodo : time := 1 ns;
-
+  constant periodo : time := 10 ns;
 begin
   -- Geração de clock
-  -- clk <= stopc and (not clk) after periodo/2;
-
-  -- Instância a ser testada
-  dut1: entity work.rom32x4(literais)   port map(a5,d4a);
-  dut2: entity work.rom32x4(de_arquivo) port map(a5,d4b);
-
+  clk <= stopc and (not clk) after periodo/2;
+  -- Instâncias a serem testada
+  dutA: entity work.rom32x4(literais)   port map(a5,d4a);
+  dutB: entity work.rom32x4(de_arquivo) port map(a5,d4b);
+  dutC: ram generic map(5,4) port map(clk,wr,a5,d4ci,d4co);
+  dutD: rom generic map(5,4,"rom32x4.dat") port map(a5,d4d);
   -- Estímulos
   stim: process
+    variable addr_tmp: bit_vector(4 downto 0);
   begin
-    -- stopc <= '1';
-
+    stopc <= '1';
+    wr <='0';
+    --! Escrevendo um padrão na RAM
+    for i in 0 to 31 loop
+      addr_tmp := bit_vector(to_unsigned(i,5));
+      d4ci <= addr_tmp(3 downto 0);
+      a5 <= addr_tmp;
+      wr<='1';
+      wait until rising_edge(clk);
+      wr<='0';
+    end loop;
+    --! Lendo todas as memórias
     for i in 0 to 31 loop
       a5 <= bit_vector(to_unsigned(i,5));
       wait for 1 ns;
@@ -57,10 +78,12 @@ begin
         report "ROM1 mem("&to_bstring(a5)&")="&to_bstring(d4a);
       assert d4b = a5(3 downto 0)
         report "ROM2 mem("&to_bstring(a5)&")="&to_bstring(d4b);
+      assert d4co = a5(3 downto 0)
+        report "RAM mem("&to_bstring(a5)&")="&to_bstring(d4co);
+      assert d4d = a5(3 downto 0)
+        report "ROM3 mem("&to_bstring(a5)&")="&to_bstring(d4d);
     end loop;
-
-    -- stopc <= '0';
-
+    stopc <= '0';
     wait;
   end process;
 end architecture;
--- vhdl_modules/memory/ram.vhd
+++ vhdl_modules/memory/ram.vhd
@@ -1,23 +1,28 @@
+--------------------------------------------------------------------------------
+--! @file   rom.vhd
+--! @brief  Parametrisable Sync RAM memory
+--! @author Bruno Albertini (balbertini@usp.br)
+--! @date   20190606
+--------------------------------------------------------------------------------
 library ieee;
 use ieee.numeric_bit.all;
-use std.textio.all;
 
 entity ram is
-  generic (
-    addressSize : natural := 64;
-    wordSize    : natural := 32
+  generic(
+    address_size : natural := 64;
+    word_size    : natural := 32
   );
-  port (
+  port(
     ck, wr : in  bit;
-    addr   : in  bit_vector(addressSize-1 downto 0);
-    data_i : in  bit_vector(wordSize-1 downto 0);
-    data_o : out bit_vector(wordSize-1 downto 0)
+    addr   : in  bit_vector(address_size-1 downto 0);
+    data_i : in  bit_vector(word_size-1 downto 0);
+    data_o : out bit_vector(word_size-1 downto 0)
   );
 end ram;
 
 architecture vendorfree of ram is
-  constant depth : natural := 2**addressSize;
-  type mem_type is array (0 to depth-1) of bit_vector(wordSize-1 downto 0);
+  constant depth : natural := 2**address_size;
+  type mem_type is array (0 to depth-1) of bit_vector(word_size-1 downto 0);
   signal mem : mem_type;
 begin
   wrt: process(ck)
